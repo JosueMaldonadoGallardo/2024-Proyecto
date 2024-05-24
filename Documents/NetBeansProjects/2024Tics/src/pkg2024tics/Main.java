10da5f6e17163fa0d08f95aa26dbac47bc36922a
Josue Maldonado <elrepoio1234@gmail.com>
2024-05-24 13:31:57 -0600

    Inicio del proyecto

---
+++ Documents/NetBeansProjects/2024Tics/src/pkg2024tics/Main.java
@@ -0,0 +1,16 @@
+//ITESS-TICS
+//ENERO- JUNIO 2024
+//CALCULO INTEGRAL
+//PORTAFOLIO
+//TI-202
+//24/05/24
+//BY AVRM Y JMG
+package pkg2024tics;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola FX");
+    }
+    
+}
